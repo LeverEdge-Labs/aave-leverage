93a97af332e7cb74c90236513a81f9310312a6f5
pythagoras <aleqvids@gmail.com>
2023-04-06 13:48:13 +0300

    update tests

--- test/shortTest.t.sol
+++ test/shortTest.t.sol
@@ -15,8 +15,8 @@ contract shortTest is Test {
     uint ethFork;
     string ETH_RPC = vm.envString("ETH_RPC");
 
-    address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address WETH = vm.envAddress("WETH_ETH");
+    address USDC = vm.envAddress("USDC_ETH");
 
     Leverage leverage;
 
